db7a309516f72c02e4930dfdf5a7d9196b1835f4
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-03-30 20:02:19 +0100

    adds Intro component

---
+++ src/Intro.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <section className="hero is-dark is-fullheight-with-navbar" id="about-me">
+      <div className="hero-body">
+        <div className="container">
+          <p className="title is-slider-right">
+            Hi there, I'm Muhammad
+            <img
+              src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif"
+              width="25px"
+            />
+          </p>
+          <div className="sub-title-wrapper">
+            <p className="sub-title">
+              I am a Full Stack Developer currently studying towards building
+              modern, fast-loading and responsive web applications. Looking for
+              an opportunity to contribute and share my passion for coding with
+              organisations that strive for cutting-edge breakthroughs.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
