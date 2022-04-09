cf01cb3b3746e504eaf355c89049b03559def293
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-10 00:35:18 +0100

    adds skeleton animation to modal on github api data fetch

--- src/pages/Modal/index.js
+++ src/pages/Modal/index.js
@@ -11,7 +11,7 @@ const Modal = () => {
   const [index, setIndex] = useState(0);
   const [isSpinning, setIsSpinning] = useState(false);
   const [tags, setTags] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   const styles = {
     hidden: {
       display: 'none',
@@ -40,7 +40,8 @@ const Modal = () => {
   }, [index, repoImages.length]);
 
   useEffect(() => {
-    getData();
+    const timeout = setTimeout(() => getData(), 1000);
+    return () => clearTimeout(timeout);
   });
 
   const getData = async () => {
@@ -50,6 +51,7 @@ const Modal = () => {
       console.log(response);
       setData(response);
       setTags(response.tags);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -61,15 +63,34 @@ const Modal = () => {
         <div className="modal-background" onClick={() => closeModal()}></div>
         <div className="card modal-content has-background-white py-5 px-5">
           <header className="card-header hero has-background-dark">
-            <h2 className="card-header-title has-text-success">
-              {parseTitle(data.title)}
+            <h2 className={'card-header-title has-text-success'}>
+              {loading ? (
+                <div className="skeleton skeleton-header"></div>
+              ) : (
+                parseTitle(data?.title || '')
+              )}
             </h2>
           </header>
           <div className="creation-date-container is-flex is-align-items-center mb-2 pt-3">
             <img className="mr-1" src="./images/calendar.svg" alt="git-tag" />
-            <p className="ml-2">Creation date: {parseDate(data.date)}</p>
+            {loading ? (
+              <div className="ml-2 skeleton skeleton-text-short"></div>
+            ) : (
+              <p className="ml-2">
+                Creation date: {parseDate(data?.date || '')}
+              </p>
+            )}
           </div>
-          <p className="repo-description">{data.description}</p>
+
+          {loading ? (
+            <>
+              <div className="skeleton skeleton-text"></div>
+              <div className="skeleton skeleton-text"></div>
+            </>
+          ) : (
+            <p className="repo-description"> {data?.description || ''}</p>
+          )}
+
           <div className="card-content p-2">
             {repoImages.map((e, i) => {
               return (
@@ -89,7 +110,9 @@ const Modal = () => {
               </p>
               <p className="card-footer-item">
                 <span>
-                  <a href={`https://github.com/moedaaboul/${data.title}`}>
+                  <a
+                    href={`https://github.com/moedaaboul/${data?.title || ''}`}
+                  >
                     <img src="./images/github.svg" alt="git-tag" />
                     Github
                   </a>
@@ -98,13 +121,21 @@ const Modal = () => {
             </footer>
             <div className="license-container is-flex is-align-items-center mb-2">
               <img src="./images/tag.svg" alt="git-tag" />
-              <p className="ml-2">
-                {tags.map((element, index) => (
-                  <span key={index} className="tag is-success m-1">
-                    {element}
-                  </span>
-                ))}
-              </p>
+              {loading ? (
+                <>
+                  <div className="ml-2 skeleton skeleton-header"></div>
+                  <div className="ml-2 skeleton skeleton-header"></div>
+                  <div className="ml-2 skeleton skeleton-header"></div>
+                </>
+              ) : (
+                <p className="ml-2">
+                  {tags.map((element, index) => (
+                    <span key={index} className="tag is-success m-1">
+                      {element}
+                    </span>
+                  ))}
+                </p>
+              )}
             </div>
             <div className="license-container is-flex is-align-items-center mb-2">
               <img
@@ -112,21 +143,35 @@ const Modal = () => {
                 className={`commit-svg ${isSpinning && `icn-spinner`}`}
                 alt="git-commit"
               />
-              <p className="ml-2" id="value" data-value={data.commits}>
-                <CountUp
-                  start={data.commits / 4}
-                  end={data.commits}
-                  duration={2}
-                  onStart={() => setIsSpinning(true)}
-                  onEnd={() => setIsSpinning(false)}
-                  className="mr-1"
-                />
-                commits
-              </p>
+              {loading ? (
+                <div className="ml-2 skeleton skeleton-text-short"></div>
+              ) : (
+                <>
+                  <p
+                    className="ml-2"
+                    id="value"
+                    data-value={data?.commits || ''}
+                  >
+                    <CountUp
+                      start={data?.commits / 4 || ''}
+                      end={data?.commits || ''}
+                      duration={2}
+                      onStart={() => setIsSpinning(true)}
+                      onEnd={() => setIsSpinning(false)}
+                      className="mr-1"
+                    />
+                    commits
+                  </p>
+                </>
+              )}
             </div>
             <div className="license-container is-flex is-align-items-center mb-2">
               <img src="./images/award.svg" alt="git-commit" />
-              <p className="ml-2">{data.license}</p>
+              {loading ? (
+                <div className="ml-2 skeleton skeleton-text-short"></div>
+              ) : (
+                <p className="ml-2">{data?.license || ''}</p>
+              )}
             </div>
           </div>
         </div>
